9bc349a508901f997c26770a0ac762d964cd25b5
SZEDER Gábor <szeder.dev@gmail.com>
2020-03-13 16:18:42 +0100

    t9999: try using only GNU coreutils, without Git's test-tool

--- t/t9999-test.sh
+++ t/t9999-test.sh
@@ -5,11 +5,13 @@ test_description='test'
 . ./test-lib.sh
 
 test_expect_success 'test' '
+	one_sec_ago=$(date -d "@$(($(date +"%s") - 1))" "+%Y%m%d%H%M%S") &&
+	touch -t ${one_sec_ago%??}.${one_sec_ago#????????????} file &&
+	old=$(date -r file "+%s") &&
 	touch file &&
-	old=$(test-tool chmtime --get =-2 file) &&
-	touch file &&
-	new=$(test-tool chmtime --get file) &&
-	test $old -lt $new
+	new=$(date -r file "+%s") &&
+
+	test "$old" != "$new"
 '
 
 test_done
